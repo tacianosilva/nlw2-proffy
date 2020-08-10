cd3dd780af60af28350bd0a0597796f71a21959d
Taciano Morais Silva <tacianosilva@gmail.com>
2020-08-10 14:01:31 -0300

    NLW Day #3

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
 
-import logoImage from '../../asserts/images/logo.svg';
+import logoIcon from '../../asserts/images/logo.svg';
 import backIcon from '../../asserts/images/icons/back.svg';
 
 import './styles.css';
@@ -11,23 +12,24 @@ interface PageHeaderProps {
   description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = (props) => {
+const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="Voltar" />
+          <img src={backIcon} alt="back" />
         </Link>
-        <img src={logoImage} alt="Proffy" />
+        <img src={logoIcon} alt="Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{props.title}</strong>
         {props.description && <p>{props.description}</p>}
+
         {props.children}
       </div>
     </header>
   );
-};
+}
 
 export default PageHeader;
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../asserts/images/logo.svg';
@@ -8,9 +8,21 @@ import studyIcon from '../../asserts/images/icons/study.svg';
 import giveClassesIcon from '../../asserts/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../asserts/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState();
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de { totalConnections } conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
 
